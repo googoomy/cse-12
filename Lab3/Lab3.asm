ec861af6236675e4b448cdc225e8a551d7889567
James Gu <ugjj2002@gmail.com>
2021-05-09 20:32:31 -0700

    Completed the rest of the program and finished up some comments

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -2,15 +2,12 @@
 
 enterHeightPrompt: .asciiz "Enter the height of the pattern (must be greater than 0):\t"
 invalidInput: .asciiz "Invalid Entry!\n"
-tab: .asciiz "\t"
-newLine: .asciiz "\n"
-asterisk: .asciiz "*\t"
 
 .text
 userInput:
-li $v0 4		#Prep to prompt the user with the enterHeightPrompt
-la $a0 enterHeightPrompt	#Put address of string into $a0
-syscall		#print
+li $v0 4			#Prep to prompt the user with the enterHeightPrompt
+la $a0 enterHeightPrompt		#Put address of string into $a0
+syscall			#print
 
 li $v0 5			#prep to get user's height
 syscall			#prompt
@@ -29,25 +26,25 @@ triangle:
 li $t1 1			#$t1 will be the counter but also the numbers along the triangle
 la $t2 ($t0)			#$t2 is set to the user's inputted height of the triangle and will be the height loop counter
 li $t3 1			#$t3 is my subtraction variable when I want to subtract 1
-li $t6 1
+li $t6 0			#$t6 is my exit variable for loopHeightTimes
 
 
 loopHeightTimes:
-beq $t6 $t2 exitHeightLoop	#exit height loop if height loop counter is zero
+beq $t6 $t2 exitHeightLoop		#exit height loop if exit variable is equal to $t2
 
-li $t7 0
+li $t7 1			#$t7 is the counter for beginning tabs loop
 loopPrintBeginningTabs:
-sub $t4 $t2 $t6		#Set $t4 equal to the value of $t2 minus one
-beq $t7 $t4 exitBeginningTabLoop	#Exit Loop if $t4 is zero
+sub $t4 $t2 $t6		#Set $t4 equal to the value of $t2 minus $t6
+beq $t7 $t4 exitBeginningTabLoop	#Exit Loop if $t7 is equal to $t4
 li $v0 11			#prep to print tab
-la $a0 9			#put address of string into $a0
+la $a0 9			#put the decimal value of tab in the ascii table into $a0
 syscall			# print tab
-addi $t7 $t7 1		#subtract 1 from $t4
+addi $t7 $t7 1		#add one to the counter
 j loopPrintBeginningTabs		#loop back
 
 exitBeginningTabLoop:
 li $v0 1			#prep to print
-la $a0 ($t1)			#put address of string into $a0
+la $a0 ($t1)			#put the value of $t1 into $a0
 syscall			# print the counter number
 
 #in between:
@@ -55,41 +52,41 @@ bne $t1 $t3 printOneTab		#if the counter number is not equal to 1 then go to pri
 j skipInBetween		#if the counter number is 1 jump to rest of code
 printOneTab:
 li $v0 11			#prep to print tab
-la $a0 9			#put address of string into $a0
+la $a0 9			#put the decimal value of tab in the ascii table into $a0
 syscall			# print tab
 
-li $s0 1
+li $s0 0			#Set $s0 to 0. It is the counter for the printing asterisks loop
 loopPrintAsterisks:
-sub $t5 $t1 $t3
-beq $s0 $t5 exitAsterisksLoop
+sub $t5 $t1 $t3		#Set the value of $t5 to the value of $t1 minus $t3
+beq $s0 $t5 exitAsterisksLoop	#If the counter $s0 is equal to $t5
 li $v0 11			#prep to print tab
-la $a0 42		#put address of string into $a0
-syscall	# print asterick
+la $a0 42			#put the decimal value of asterisk in the ascii table into $a0
+syscall			# print asterick
 li $v0 11			#prep to print tab
-la $a0 9			#put address of string into $a0
+la $a0 9			#put the decimal value of tab in the ascii table into $a0
 syscall			# print tab
-addi $s0 $s0 1
-j loopPrintAsterisks
+addi $s0 $s0 1		#increment the counter by one
+j loopPrintAsterisks		#jump back to continue the loop
 
 exitAsterisksLoop:
-addi $t1 $t1 1
-li $v0 1			#prep to print tab
-la $a0 ($t1)		#put address of string into $a0
+addi $t1 $t1 1		#increment the counter number by one
+li $v0 1			#prep to print the counter number
+la $a0 ($t1)			#put the value of the counter number into $a0
 syscall			# print counter number
 
 
 skipInBetween:
 addi $t1 $t1 1		#add one to the counter number
 
-li $v0 11			#prep to print
-la $a0 10		#put address of string into $a0
-syscall			#print newLine
+li $v0 11			#prep to print new line
+la $a0 10			#put the decimal value of new line in the ascii table into $a0
+syscall			#print new line
 
-addi $t6 $t6 1
+addi $t6 $t6 1		#increment the height times loop counter by one
 	
-j loopHeightTimes
+j loopHeightTimes		#continue looping in the height times loop
 
 exitHeightLoop:
 
-li $v0 10		#prep to exit
-syscall		#exit
+li $v0 10			#prep to exit
+syscall			#exit
