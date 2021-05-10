12a26bf309df9c88aa48f5d5429e73ae14712f14
James Gu <ugjj2002@gmail.com>
2021-05-09 19:50:19 -0700

    got the code to run by fixing my for loops

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -27,23 +27,26 @@ j userInput			#restart at beginning because of invalid input
 
 triangle:
 li $t1 1			#$t1 will be the counter but also the numbers along the triangle
-la $t2 ($t0)			#$t2 is set to the user's inputted height of the triangle
+la $t2 ($t0)			#$t2 is set to the user's inputted height of the triangle and will be the height loop counter
 li $t3 1			#$t3 is my subtraction variable when I want to subtract 1
+li $t6 1
+li $t7 1
+li $s0 1
 
 loopHeightTimes:
-beq $t2 $zero exitHeightLoop
+beq $t6 $t2 exitHeightLoop	#exit height loop if height loop counter is zero
 
 loopPrintBeginningTabs:
 sub $t4 $t2 $t3		#Set $t4 equal to the value of $t2 minus one
-beq $t4 $zero exitBeginningTabLoop	#Exit Loop if $t4 is zero
-li $v0 4			#prep to print tab
-la $a0 tab			#put address of string into $a0
+beq $t7 $t4 exitBeginningTabLoop	#Exit Loop if $t4 is zero
+li $v0 11			#prep to print tab
+la $a0 9			#put address of string into $a0
 syscall			# print tab
-sub $t4 $t4 $t3		#subtract 1 from $t4
+addi $t7 $t7 1		#subtract 1 from $t4
 j loopPrintBeginningTabs		#loop back
 
 exitBeginningTabLoop:
-li $v0 4			#prep to print
+li $v0 1			#prep to print
 la $a0 ($t1)			#put address of string into $a0
 syscall			# print the counter number
 
@@ -51,22 +54,25 @@ syscall			# print the counter number
 bne $t1 $t3 printOneTab		#if the counter number is not equal to 1 then go to print one tab
 j skipInBetween		#if the counter number is 1 jump to rest of code
 printOneTab:
-li $v0 4			#prep to print tab
-la $a0 tab			#put address of string into $a0
+li $v0 11			#prep to print tab
+la $a0 9			#put address of string into $a0
 syscall			# print tab
 
 loopPrintAsterisks:
 sub $t5 $t1 $t3
-beq $t5 $zero exitAsterisksLoop
-li $v0 4			#prep to print tab
-la $a0 asterisk		#put address of string into $a0
-syscall			# print asterick
-sub $t5 $t5 $t3
+beq $s0 $t5 exitAsterisksLoop
+li $v0 11			#prep to print tab
+la $a0 42		#put address of string into $a0
+syscall	# print asterick
+li $v0 11			#prep to print tab
+la $a0 9			#put address of string into $a0
+syscall			# print tab
+addi $s0 $s0 1
 j loopPrintAsterisks
 
 exitAsterisksLoop:
 addi $t1 $t1 1
-li $v0 4			#prep to print tab
+li $v0 1			#prep to print tab
 la $a0 ($t1)		#put address of string into $a0
 syscall			# print counter number
 
@@ -74,11 +80,11 @@ syscall			# print counter number
 skipInBetween:
 addi $t1 $t1 1		#add one to the counter number
 
-li $v0 4			#prep to print
-la $a0 newLine		#put address of string into $a0
+li $v0 11			#prep to print
+la $a0 10		#put address of string into $a0
 syscall			#print newLine
 
-sub $t2 $t2 $t3
+addi $t6 $t6 1
 j loopHeightTimes
 
 exitHeightLoop:
